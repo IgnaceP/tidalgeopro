5b8f76719136e2467d305a067869b878051282b1
Olivier Gourgue <ogourgue@gmail.com>
2020-05-14 19:42:18 -0400

    Update skeleton.py
    calculate channel width as twice the distance between skeleton and channel edge

--- skeleton.py
+++ skeleton.py
@@ -818,3 +818,34 @@ def downstream_distance(coords, sections, mls, mpol, dns, \
   mls_new = MultiLineString(lss)
 
   return coords_new, dist, sections_new, mls_new
+
+
+
+################################################################################
+# channel width ################################################################
+################################################################################
+
+def channel_width(coords, mpol):
+
+  """ Compute channel width along skeleton, as two times the distance between skeleton and channel edges
+
+  Required parameters:
+  coords (Numpy array of shape (n, 2)): coordinates of the skeleton points
+  mpol (MultiPolygon): structure of multiple Polygons describing channel edges
+
+  Returns:
+  Numpy array of shape (n): channel width at each skeleton point
+
+  """
+
+  # initialize
+  width = np.zeros(coords.shape[0])
+
+  # compute channel width
+  for i in range(len(width)):
+    point = Point(coords[i, :])
+    width[i] = point.distance(mpol.boundary) * 2
+
+  return width
+
+
