c7fa817c54f4bc1e8ccedc701b55b3840ff85c91
Olivier Gourgue <ogourgue@gmail.com>
2020-05-22 16:37:58 -0400

    Update cross_section.py
    added defintion of the function metrics to calculate channel width, depth and cross section area

--- cross_section.py
+++ cross_section.py
@@ -9,7 +9,10 @@ from shapely.geometry import Point
 
 def channel_width(coords, mpol):
 
-  """ Compute channel width along skeleton, as two times the distance between skeleton and channel edges
+  """ Compute channel width along skeleton, as twice the distance between the skeleton and the channel edges.
+
+  !!! Attention !!!
+  This is the distance to the closest point defining the channel edges. A more accurate calculation can be found in the channelwidth module. The present function gives satisfactory results for high resolution channel edges.
 
   Required parameters:
   coords (Numpy array of shape (n, 2)): coordinates of the skeleton points
@@ -31,3 +34,37 @@ def channel_width(coords, mpol):
   return width
 
 
+################################################################################
+# cross-section metrics ########################################################
+################################################################################
+
+def metrics(coords, sections, mpol, slf_fn):
+
+  """ Compute cross-section metrics (channel width, depth and cross-section area) along the skeleton, based on channel edges and a digital elevation model (DEM).
+
+  !!! Attention !!!
+  Currently, the DEM must be defined on a Selafin file (format of Telemac output files), which assumes an unstructured triangular grid. Other formats (including for structured rectangular grids) will be considered in the future.
+
+  Required parameters:
+  coords (Numpy array of shape (n, 2)): coordinates of the skeleton points
+  sections (Numpy array of shape (n)): section index of each skeleton point
+  mpol (MultiPolygon): structure of multiple Polygons describing channel edges
+  slf_fn (string): name of the Selafin file describing the DEM
+
+  Returns
+  Numpy array of shape (n): channel width at each skeleton point
+  Numpy array of shape (n): channel depth at each skeleton point
+  Numpy array of shape (n): channel cross-section area at each skeleton point
+
+  """
+
+  for i in np.unique(sections):
+    print(i)
+
+
+
+
+
+
+
+  #return width, depth, area
